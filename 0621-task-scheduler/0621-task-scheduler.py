5feb36e78fda7531f30d80e88377a1afb19348be
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-05-29 20:47:33 +0800

    Time: 2857 ms (5.01%), Space: 20.9 MB (15.23%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,27 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        task_count = Counter(tasks)
+        pq = []
+        for task, count in task_count.items():
+            heapq.heappush(pq, (-count, task))
+
+        cooldown_tasks = {}
+        intervals = 0
+        while pq or cooldown_tasks:
+            intervals += 1
+            for t in cooldown_tasks:
+                if cooldown_tasks[t] == 0 and t[0]+1 < 0:
+                    heapq.heappush(pq, (t[0]+1, t[1]))
+                cooldown_tasks[t] -= 1
+                
+            cooldown_tasks = {k: v for k, v in cooldown_tasks.items() if v >= 0 and k[0] < -1}
+
+            if pq:
+                next_task = heapq.heappop(pq)
+                if next_task[0] < 0:
+                    cooldown_tasks[next_task] = n
+            else:
+                print(intervals, "skip")
+        return intervals-1
+        
+        
