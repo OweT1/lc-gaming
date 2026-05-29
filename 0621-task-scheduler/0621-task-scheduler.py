4993d34f948a48f102475c3602205e611f64a37a
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-05-29 20:48:32 +0800

    Time: 2326 ms (5.01%), Space: 20.9 MB (39.5%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -14,7 +14,7 @@ class Solution:
                     heapq.heappush(pq, (t[0]+1, t[1]))
                 cooldown_tasks[t] -= 1
                 
-            cooldown_tasks = {k: v for k, v in cooldown_tasks.items() if v >= 0 and k[0] < -1}
+            cooldown_tasks = {k: v for k, v in cooldown_tasks.items() if v >= 0 and k[0] < -1} # only add to cooldown if there is a need to execute the task again
 
             if pq:
                 next_task = heapq.heappop(pq)
