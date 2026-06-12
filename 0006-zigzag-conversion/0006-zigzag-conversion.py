9d8abcdedf1a6fc270212c8facbabde0dd702139
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-06-13 01:00:20 +0800

    Time: 413 ms (5.03%), Space: 22.1 MB (5.03%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -2,17 +2,17 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s
-        items = [""] * numRows
+        items = [[] for _ in range(numRows)]
         for i, c in enumerate(s):
             j = i % (2 * numRows - 2)
             if j < numRows:
-                items[j] += c
+                items[j].append(c)
             else:
                 for k in range(len(items)):
                     if k == 2 * numRows - j - 2:
-                        items[k] += c
+                        items[k].append(c)
                     else:
-                        items[k] += " "
-        return "".join(items).replace(" ", "")
+                        items[k].append(" ")
+        return "".join(["".join(i) for i in items]).replace(" ", "")
 
         
