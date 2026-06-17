2c48aea83c9ff06ad3a2da49fbb86327b2ba7eb9
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-06-18 00:37:58 +0800

    Time: 670 ms (38.37%), Space: 35.1 MB (11.78%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,6 +1,7 @@
 class Solution:
-    def mergeArray(self, dq1: deque[int], dq2: deque[int]) -> deque[int]:
-        res = deque()
+    def mergeArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        res = []
+        dq1, dq2 = deque(arr1), deque(arr2)
         while dq1 and dq2:
             if dq1[0] < dq2[0]:
                 res.append(dq1.popleft())
@@ -14,9 +15,9 @@ class Solution:
         return res
 
     def sortArray(self, nums: List[int]) -> List[int]:
-        arrs = [deque([n]) for n in nums]
+        arrs = [[n] for n in nums]
         while len(arrs) > 1:
             arrs = [self.mergeArray(arrs[i], [] if i == len(arrs) - 1 else arrs[i+1]) for i in range(0, len(arrs), 2)]
-        return list(arrs[0])
+        return arrs[0]
 
         
