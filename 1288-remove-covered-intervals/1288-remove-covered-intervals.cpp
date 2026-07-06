6a4d02aff610d35877b127b68201ab26cd2d59d6
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-07-06 19:35:27 +0800

    Time: 173 ms (5.03%), Space: 34.1 MB (5.3%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         std::sort(intervals.begin(), intervals.end(), [](const vector<int> a, const vector<int> b){
-            return a[1] <= b[1];
-        });
-        std::sort(intervals.begin(), intervals.end(), [](const vector<int> a, const vector<int> b){
-            return a[0] <= b[0];
+            if (a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
         });
 
         int res = 0, r = 0;
         for (vector<int> x: intervals) {
             res += x[1] > r;
             r = max(r, x[1]);
+            for (int k: x) std::cout << k << " ";
+            std::cout << "\n";
         }
 
         return res;
