7d360d5aaa4641b3d39a2d5a75f666c7e298e391
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-07-10 16:07:40 +0800

    Time: 111 ms (54.03%), Space: 49.4 MB (79.84%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+        component_groups = [1]*n
+
+        for i in range(1, n):
+            component_groups[i] = component_groups[i-1] + (1 if nums[i] - nums[i-1] > maxDiff else 0)
+
+        print(component_groups)
+        res = []
+        for u, v in queries:
+            res.append(component_groups[u] == component_groups[v])
+ 
+        return res
+             
+        
