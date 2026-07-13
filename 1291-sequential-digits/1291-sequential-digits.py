c57d6ee0c95fcd044c28e70a43bc0ec59dc0f273
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-07-13 18:14:51 +0800

    Time: 0 ms (100%), Space: 19.4 MB (30.59%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        
+        def generateInteger(start: int, length: int):
+            base = start * 10 **(length-1)
+            for i in range(1, length):
+                base += (start+i) * 10**(length-i-1)
+            return base
+        
+        digit_lengths = range(len(str(low)), len(str(high))+1)
+        output = []
+        for length in digit_lengths:
+            start_digit = 1
+            while start_digit + length <= 10:
+                res = generateInteger(start_digit, length)
+                if res >= low and res <= high:
+                    output.append(res)
+                start_digit += 1
+        return output
+
