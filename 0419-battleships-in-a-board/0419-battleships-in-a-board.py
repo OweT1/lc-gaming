cf26b5dd2b1b6f14c543b2a696f6a47e1e2f2d36
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-08-13 00:33:18 +0800

    Time: 8 ms (7.93%), Space: 21.2 MB (50.74%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -0,0 +1,34 @@
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        n_rows, n_cols = len(board), len(board[0])
+
+        visited = [[0]*n_cols for _ in range(n_rows)]
+        for b in board:
+            print(b)
+        res = 0
+        for r in range(n_rows):
+            for c in range(n_cols):
+                if board[r][c] == 'X' and not visited[r][c]:
+                    queue = [(r, c)]
+                    while queue:
+                        t_r, t_c = queue.pop()
+                        for direction in DIRECTIONS:
+                            new_r, new_c = t_r + direction[0], t_c + direction[1]
+                            if (
+                                new_r >= 0 and
+                                new_r < n_rows and
+                                new_c >= 0 and
+                                new_c < n_cols and
+                                not visited[new_r][new_c] and
+                                board[new_r][new_c] == 'X'
+                            ):
+                                queue.append((new_r, new_c))
+                                visited[new_r][new_c] = 1
+                    res += 1
+                    print()
+        return res
+
+
+
+        
