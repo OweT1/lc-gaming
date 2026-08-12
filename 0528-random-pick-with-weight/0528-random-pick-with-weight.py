b3022651491a2f7b653705ad7960ba9c5dccb2ec
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-08-13 00:04:50 +0800

    Time: 37 ms (92.94%), Space: 24.7 MB (46.86%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -5,30 +5,11 @@ class Solution:
 
     def _calc_sum(self):
         self.w_sum = sum(self.w)
-        cumu_sum = [0, self.w[0]]
+        cumu_sum = [self.w[0]]
         for i in range(1, len(self.w)):
             cumu_sum.append(cumu_sum[-1] + self.w[i])
         self.w_cumu_sum = cumu_sum
 
-    def binary_search(self, v: int):
-        l, r = 0, len(self.w_cumu_sum) - 1
-        m = (l + r) // 2
-        change = True
-        while change:
-            change = False
-            if v == self.w_cumu_sum[m]:
-                print(m)
-                return m
-            elif v > self.w_cumu_sum[m]:
-                change = l != m
-                l = m
-                m = (l + r) // 2
-            else:
-                change = r != m
-                r = m
-                m = (l + r) // 2
-        return m
-
     def pickIndex(self) -> int:
-        rand_val = random.randint(0, self.w_sum-1)
-        return self.binary_search(rand_val)
+        rand_val = random.randint(1, self.w_sum)
+        return bisect.bisect_left(self.w_cumu_sum, rand_val)
