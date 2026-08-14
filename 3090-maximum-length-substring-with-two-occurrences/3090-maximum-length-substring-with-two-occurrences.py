576b9bf591f63764b8bf2265e732f3f50591b8da
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-08-14 09:08:02 +0800

    Time: 3 ms (76.16%), Space: 19 MB (98.37%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
@@ -7,12 +7,10 @@ class Solution:
 
         while r < len(s):
             c_counter[s[r]] = c_counter.get(s[r], 0) + 1
-
             while c_counter[s[r]] == k+1:
                 max_len = max(max_len, r-l)
                 c_counter[s[l]] -= 1
                 l += 1
-
             r += 1
         return max(max_len, r-l)
 
