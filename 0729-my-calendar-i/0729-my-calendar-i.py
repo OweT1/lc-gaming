ada41e4b08e22538877786d4a95835b40d8f163d
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-08-17 14:16:56 +0800

    Time: 33 ms (87.61%), Space: 20.3 MB (25.4%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -0,0 +1,23 @@
+class MyCalendar:
+
+    def __init__(self):
+        self.bookings = []
+
+    def book(self, startTime: int, endTime: int) -> bool:    
+        pos = bisect.bisect_left(self.bookings, (startTime, endTime))
+        print(pos)
+        if self.bookings and \
+            (
+                (pos > 0 and startTime < self.bookings[pos-1][1]) or \
+                (pos <= len(self.bookings)-1 and endTime > self.bookings[pos][0])
+            ):
+            return False
+        else:
+            self.bookings.insert(pos, (startTime, endTime))
+            return True
+        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(startTime,endTime)
