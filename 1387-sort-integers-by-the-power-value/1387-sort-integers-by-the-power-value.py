89f28b3cb62f00faf9113807d0dc6e5f4e3169a9
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-08-21 17:50:11 +0800

    Time: 89 ms (75.85%), Space: 45.6 MB (21.95%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -1,28 +1,24 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
         
-        cache = {}
+        cache = {1: 0}
         def solve(x: int) -> int:
             res = 0
 
-            if x in cache:
-                return cache[x]
-            y = x
-            while y > 1:
-                if y in cache:
-                    return res + cache[y]
-                if y % 2 == 0:
-                    y = y // 2
-                else:
-                    y = y*3 + 1
-                res += 1
+            if x in cache: return cache[x]
+            
+            if x % 2 == 0:
+                res = solve(x // 2) + 1
+            else:
+                res = solve(3*x + 1) + 1
+
             cache[x] = res
             return res
         
         pq = []
         for i in range(lo, hi+1):
             heapq.heappush(pq, (solve(i), i))
-        
+
         for _ in range(k-1):
             heapq.heappop(pq)
         return heapq.heappop(pq)[1]
