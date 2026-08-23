8c26b1280a00300a92e0c7919a82e593df631477
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-08-23 22:26:47 +0800

    Time: 39 ms (94.36%), Space: 20.4 MB (86.62%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,48 +1,31 @@
 class Solution:
-    def is_linked(self, s1: str, s2: str) -> bool:
-        is_off = False
-        for c1, c2 in zip(s1, s2):
-            if c1 != c2 and is_off:
-                return False
-            elif c1 != c2:
-                is_off = True
-        return True
-    
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList: return 0
+        wordSet = set(wordList)
+        if endWord not in wordSet: return 0
 
-        neighbours = {}
-        for i in range(len(wordList)):
-            for j in range(i+1, len(wordList)):
-                word_i, word_j = wordList[i], wordList[j]
-                if self.is_linked(word_i, word_j):
-                    if word_i not in neighbours:
-                        neighbours[word_i] = []
-                    if word_j not in neighbours:
-                        neighbours[word_j] = []
-                    neighbours[word_i].append(word_j)
-                    neighbours[word_j].append(word_i)
+        beginSet = {beginWord}
+        endSet = {endWord}
+        visited = set()
+        turns = 1 # we want number of words
 
-        nearest = [endWord]
-        visited = set(nearest)
-        # 1 is because we want number of words, which is number of transformations + 1
-        turn = 1
-        candidates = set([word for word in wordList if self.is_linked(beginWord, word)])
-        while nearest:
-            temp = []
-            for node in nearest:
-                if node == beginWord:
-                    return turn
-                elif node in candidates:
-                    return turn + 1
-                visited.add(node)
-                if node not in neighbours: continue
+        while beginSet and endSet:
+            if len(beginSet) > len(endSet):
+                beginSet, endSet = endSet, beginSet
+            
+            nextSet = set()
+            for word in beginSet:
+                for i in range(len(word)):
+                    for j in range(26):
+                        c = chr(ord('a')+j)
+                        if c == word[i]: continue
 
-                for n in neighbours[node]:
-                    if n not in visited:
-                        temp.append(n)
-                        visited.add(n)
-
-            nearest = temp.copy()
-            turn += 1
+                        new_word = word[:i] + c + word[i+1:]
+                        if new_word in endSet:
+                            return turns + 1
+                        
+                        if new_word in wordSet and new_word not in visited:
+                            nextSet.add(new_word)
+                            visited.add(new_word)
+            beginSet = nextSet
+            turns += 1
         return 0
