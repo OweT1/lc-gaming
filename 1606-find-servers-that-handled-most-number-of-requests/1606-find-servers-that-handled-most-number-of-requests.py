f6381d704fab3fda8eebb93f96451bf3cbf624e6
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-08-24 18:23:17 +0800

    Time: 259 ms (83.55%), Space: 46 MB (48.71%) - LeetHub

--- 1606-find-servers-that-handled-most-number-of-requests/1606-find-servers-that-handled-most-number-of-requests.py
+++ 1606-find-servers-that-handled-most-number-of-requests/1606-find-servers-that-handled-most-number-of-requests.py
@@ -1,31 +1,26 @@
 class Solution:
     def busiestServers(self, k: int, arrival: List[int], load: List[int]) -> List[int]:
-        before = list(range(k)) # will reverse on first call
+        before = list(range(k))
         after = []
-        busy = []
-        server_requests = [0]*k
+        busy = [] # to be flipped
+        server_counter = [0]*k
 
-        for i, arr in enumerate(arrival):
+        for i, (arr, ld) in enumerate(zip(arrival, load)):
             server_id = i % k
-            if server_id == 0:
-                after = before
-                before = []
-    
-            # Free busy servers if any
+            if server_id == 0: before, after = [], before
+
             while busy and busy[0][0] <= arr:
-                _, s = heapq.heappop(busy)
-                if s < server_id:
-                    heapq.heappush(before, s)
-                else:
-                    heapq.heappush(after, s)
-            
+                _, s_id = heapq.heappop(busy)
+                if s_id < server_id: heapq.heappush(before, s_id)
+                else: heapq.heappush(after, s_id)
+
             available = after if after else before
             if not available: continue
 
-            new_server = heapq.heappop(available)
-            heapq.heappush(busy, (arr + load[i], new_server))
-            server_requests[new_server] += 1
-        
-        max_requests = max(server_requests)
-        return [i for i, handled in enumerate(server_requests) if handled == max_requests]
-                    
+            to_add = heapq.heappop(available)
+            heapq.heappush(busy, (arr+ld, to_add))
+            server_counter[to_add] += 1
+        max_count = max(server_counter)
+        return [i for i, num in enumerate(server_counter) if num == max_count]
+
+
