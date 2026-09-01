7523a8b9f464344c1406361a1da899da1f230f20
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-09-01 23:18:16 +0800

    Time: 5 ms (53.97%), Space: 20.5 MB (62.48%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        neighbours = defaultdict(list)
+        in_deg = defaultdict(int)
+
+        for prereq in prerequisites:
+            u, v = prereq
+            in_deg[v] += 1
+            neighbours[u].append(v)
+        
+        dq = deque([i for i in range(numCourses) if i not in in_deg])
+        coursesTaken = 0
+        while dq:
+            ele = dq.popleft()
+            ele_nb = neighbours.get(ele, [])
+            for nb in ele_nb:
+                in_deg[nb] -= 1
+                if in_deg[nb] == 0:
+                    dq.append(nb)
+            coursesTaken += 1
+        
+        return coursesTaken == numCourses
+        
