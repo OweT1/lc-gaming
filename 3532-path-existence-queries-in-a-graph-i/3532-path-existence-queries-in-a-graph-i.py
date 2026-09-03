a95acb9634529740b8f676f059c88f5bd0b04aa3
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-09-03 23:31:27 +0800

    Time: 323 ms (10.97%), Space: 49.5 MB (82.81%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
@@ -1,14 +1,36 @@
+class UF:
+    def __init__(self, size: int):
+        self.size = size
+        self.parents = list(range(size))
+        self.rank = [1]*size
+    
+    def find(self, node: int) -> int:
+        if node == self.parents[node]: return node
+
+        self.parents[node] = self.find(self.parents[node])
+        return self.parents[node]
+
+    def union(self, n1: int, n2: int) -> bool:
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2: return False
+
+        if self.rank[p1] > self.rank[p2]:
+            self.parents[n2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.parents[n1] = p2
+            self.rank[p2] += self.rank[p1]
+        return True
+
 class Solution:
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
-        component_groups = [1]*n
-
-        for i in range(1, n):
-            component_groups[i] = component_groups[i-1] + (1 if nums[i] - nums[i-1] > maxDiff else 0)
-
+        uf = UF(n)
+        for i in range(n-1):
+            if nums[i+1] - nums[i] <= maxDiff:
+                uf.union(i, i+1)
         res = []
-        for u, v in queries:
-            res.append(component_groups[u] == component_groups[v])
- 
-        return res
-             
+        for q in queries:
+            n1, n2 = q
+            res.append(True) if uf.find(n1) == uf.find(n2) else res.append(False)
+        return res   
         
