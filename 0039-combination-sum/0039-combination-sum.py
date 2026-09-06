f80b9fdf0addba90276ebf643cc132fa9fadda42
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-09-07 00:57:10 +0800

    Time: 11 ms (46.66%), Space: 19.6 MB (65.89%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,20 +1,20 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = set()
+        res = []
         temp = []
 
-        def backtrack(curr: int):
+        def backtrack(i: int, curr: int):
+            if i >= len(candidates) or curr > target: return
             if curr == target:
                 nonlocal res
-                res.add(tuple(sorted(temp)))
+                res.append(temp.copy())
                 return
             
-            for cand in candidates:
-                if curr + cand <= target:
-                    temp.append(cand)
-                    backtrack(curr+cand)
-                    temp.pop()
-
-        backtrack(0)
-        return list(res)
+            temp.append(candidates[i])
+            backtrack(i, curr + candidates[i])
+            temp.pop()
+            backtrack(i+1, curr)
+        
+        backtrack(0, 0)
+        return res
 
