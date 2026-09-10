afb9567ba40300de90dcfdcc497656f4a7d47d7e
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2026-09-10 23:29:02 +0800

    Time: 4 ms (39.82%), Space: 20.6 MB (5.13%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,11 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+    
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def add(self, word: str):
+        temp = self.root
+        for c in word:
+            if c not in temp.children: temp.children[c] = TrieNode()
+            temp = temp.children[c]
+        temp.is_end = True
+
+    def find(self, word: str) -> bool:
+        temp = self.root
+        for c in word:
+            if c not in temp.children: return False
+            temp = temp.children[c]
+        return temp.is_end
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [True] + [False] * len(s)
+        trie = Trie()
+        for word in wordDict:
+            trie.add(word)
         
-        for i in range(1, len(s)+1):
-            for word in wordDict:
-                start = i - len(word)
-                if start >= 0 and dp[start] and s[start:i] == word:
-                    dp[i] = True
-        
+        n = len(s)
+        dp = [False]*n
+        for i in range(n):
+            if i == 0 or dp[i-1]: # check for each pos in s if it starts with any word
+                curr = trie.root
+                for j in range(i, n):
+                    c = s[j]
+                    if c not in curr.children: break
+
+                    curr = curr.children[c]
+                    if curr.is_end: dp[j] = True
         return dp[-1]
